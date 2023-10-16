cc807a0b4c1133a4c1997769eb9317a4e52a9686
Adriano Medeiros <123388123+AdrianoMedeirosDev@users.noreply.github.com>
2023-10-16 15:58:05 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector ('header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+function abrirModal() {
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+}
